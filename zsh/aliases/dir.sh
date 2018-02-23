db9449e23ae83d8863c72d7fbcd8722ff072aa96
HDBandit <gerard.vico@letgo.com>
2018-02-23 15:56:55 +0100

    Split aliases and export

---
+++ zsh/aliases/dir.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+alias chat="cd ~/letgo/chat"
+alias hitch="cd ~/letgo/hitch"
+alias dc="cd ~/letgo/chat/docker"
+alias p='pwd'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias ..2='cd ../..'
+alias ..3='cd ../../..'
+alias ..4='cd ../../../..'
+alias ll='exa -l --git'
+alias la='exa -la --git'
+alias lf='ls -d */'
+alias ctree='exa -T'
---
+++ zsh/exports/exports.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+export ZSH=/Users/gerard/.oh-my-zsh
+export FZF_DEFAULT_OPTS='--color=bg+:24 --reverse'
+export JAVA_OPTS="-Duser.timezone=UTC"
